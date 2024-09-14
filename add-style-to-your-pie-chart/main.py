aaea89c43f2b1ebe93b3d482bb322477d3a68a2d
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2024-09-13 22:10:45 -0500

    Edited colors and title font size

--- add-style-to-your-pie-chart/main.py
+++ add-style-to-your-pie-chart/main.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 
 snack_scores = [80,45,34]
+colors = ["#DC143C", "#6495ED", "#7FFF00" ]
 
 slice_labels = ["Chocolate", "Cheese", "Pickles"]
 
-plt.pie(snack_scores, labels=slice_labels)
+plt.pie(snack_scores, labels=slice_labels, colors=colors)
 
-plt.title("Snack Scores")
+plt.title("Snack Scores", fontsize=30)
 
 plt.savefig("snack_scores.png")
