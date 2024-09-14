e4e20f573676c4228a9f130101aaaa7cefa951b4
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2024-09-13 21:55:13 -0500

    Initial commit

---
+++ create-your-first-data-visualization/main.py
@@ -0,0 +1,16 @@
+# Import matplotlib library here
+import matplotlib.pyplot as plt
+
+# Let's rank some of our favorite snacks
+snack_scores = [85, 40, 90]
+slice_labels = ["chips", "nuts", "cheese puffs"]
+
+
+# Let's make a pie chart!
+plt.pie(snack_scores, labels=slice_labels)
+
+# Give your pie chart a title in the quotes
+plt.title("Bryan's Favorite Snacks")
+
+# Put the name of your file in the quotes and give it a .png extension
+plt.savefig("bryans_snacks")
