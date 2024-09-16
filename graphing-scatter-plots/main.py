4c4cf072e7e57fc657431b4f3eb5a33c9b32bacf
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2024-09-16 12:58:23 -0500

    Added code to build a scatter plot

--- graphing-scatter-plots/main.py
+++ graphing-scatter-plots/main.py
@@ -1,5 +1,6 @@
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
 
 with open("tips.csv", "r") as file:
   data = csv.reader(file,delimiter=",")
@@ -16,3 +17,9 @@ print(f"The average bill amount is ${round(np.mean(bills), 2)}")
 print(f"The median bill amount is ${round(np.median(bills), 2)}")
 print(f"The smallest bill is ${round(np.min(bills), 2) }")
 print(f"The largest bill is ${round(np.max(bills), 2)}")
+
+plt.scatter(size, tip_percentages,color="#483d8b")
+plt.xlabel("Dinner Party Size")
+plt.ylabel("Tip Percentage")
+plt.title("Tip Percentage vs. Party Size")
+plt.savefig("tip_vs_party_size.png")
