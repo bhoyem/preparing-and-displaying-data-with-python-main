f70ad03f460945649c10c52eeaf0e16c0620ba07
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2024-09-20 20:31:27 -0500

    Added code to create a heatmap showing party size vs avg tip received

--- creating-a-heatmap/main.py
+++ creating-a-heatmap/main.py
@@ -2,3 +2,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+with open("tips.csv","r") as csvfile:
+    tips = pd.read_csv(csvfile, delimiter=",")
+    
+tips_pivoted = tips.pivot_table(
+    values = "tip",
+    index = ["size"],
+    columns = ["time"]
+)
+
+fig = sns.heatmap(tips_pivoted, annot=True, cmap="YlGn")
+fig.set_ylim(0,6)
+
+plt.xlabel("Tip")
+plt.ylabel("Party Size")
+plt.title("Party Size vs Average Tip Received")
+
+plt.savefig("tips_vs_partysize.png")
+    
