4e4a930b4f5001a00154339073e049d9d0a86d0f
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2024-09-22 00:03:17 -0500

    Added mosaic plot to summarize endangered species data

--- making-a-mosaic-plot/main.py
+++ making-a-mosaic-plot/main.py
@@ -1,5 +1,14 @@
+import matplotlib.pyplot as plt
+import pandas as pd
 import re
 import json
+import seaborn as sns
+from statsmodels.graphics.mosaicplot import mosaic
+
+classes = ["Mammalia", "Aves", "Reptilia"]
+statuses = ["Endangered", "Critically endangered", "Vulnerable"]
+
+mosaic_data = []
 
 with open("data.json", "r") as text:
     data = json.load(text)
@@ -7,4 +16,30 @@ with open("data.json", "r") as text:
 for item in data:
   item["Category"] = re.compile(" [\.(]").split(item["Category"])[0]
 
-print(data)
+for item in data:
+   if item["Animal Class"] in classes and item["Category"] in statuses:
+      mosaic_data.append(item)
+
+properties = {
+   "Endangered": {"color": "#FACDB6"},
+   "Critically endangered": {"color": "#C5CADE"},
+   "Vulnerable": {"color": "#A8DBD2"}
+}
+
+plt.rc("font", size=8)
+
+mosaic_dataframe = pd.DataFrame(mosaic_data)
+
+fig = mosaic(
+   mosaic_dataframe, 
+   ["Category", "Animal Class"], 
+   title="Conservation Status by Animal Class", 
+   gap=[0.02, 0.02], 
+   axes_label=True, 
+   properties=lambda x: properties[x[0]])
+
+plt.savefig("endangered-species-mosaic.png")
+
+
+
+# print(data)
