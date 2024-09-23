be66630be4a80f1c0462cb2e5e8a285d2f44f75a
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2024-09-23 11:19:40 -0500

    Added code to display average book rating against number of pages using a bubble plot

--- choose-your-own-data-visualization-adventure/main.py
+++ choose-your-own-data-visualization-adventure/main.py
@@ -1,8 +1,25 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import csv
 
-with open("books.csv","r") as datafile:
+with open("books.csv","r", encoding="utf-8") as datafile:
     data = pd.read_csv(datafile,delimiter=",")
 
-print(data["language_code"])
+fig = sns.scatterplot(
+    x="# num_pages", 
+    y="average_rating", 
+    hue="average_rating", 
+    size="# num_pages", 
+    sizes=(20,350), 
+    data=data, 
+    palette="blend:#E55648,#50B131",
+    legend="auto")
+
+plt.title("Book Avg Ratings vs Page Count")
+plt.xlabel("Number of Pages")
+plt.ylabel("Average Rating")
+
+plt.savefig("avg-book-ratings.png")
+# print(data["language_code"])
+
